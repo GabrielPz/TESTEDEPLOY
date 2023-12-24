2e9eafd756845678c808edfbe8630bd098b6bcb7
Gabriel <gabrielhenriquedps@gmail.com>
2023-12-24 10:05:38 -0300

    Fix!

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
-  distDir: 'build',
-  // output: 'export'
-  // pageExtensions: ['page.tsx', 'page.ts, api.tsx', 'api.ts'],
 }
 
 module.exports = nextConfig
